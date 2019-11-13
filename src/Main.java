1581c04bea5e66d0fb46f7265aa8e14fe98c198e
hassan-mourad-mohamed <43889464+hassan-mourad-mohamed@users.noreply.github.com>
2019-11-13 15:26:12 +0200

    added "add" method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    public static int add(int x,int y) {
+        return x+y;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }

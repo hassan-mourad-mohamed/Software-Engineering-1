e3a0cc1d56d67dd04c2a03c0459ec4f93b24bff2
hassan-wolverine <hassanwolverin@gmail.com>
2019-11-13 15:53:49 +0200

    added "subtract" method

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static int add(int number1,int number2) {
         return number1+number2;
     }
-    
+
+    public static int subtract(int number1,int number2) {
+        return number1-number2;
+    }
+
     public static void main(String[] args) {
     }
 }
